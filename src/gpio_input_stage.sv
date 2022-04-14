1ec2c23a00dd45df92fe562937d89574b8e47636
Manuel Eggimann <meggimann@iis.ee.ethz.ch>
2022-04-14 09:04:49 +0200

    Add license file and missing file header

--- src/gpio_input_stage.sv
+++ src/gpio_input_stage.sv
@@ -1,3 +1,36 @@
+//-----------------------------------------------------------------------------
+// Title         : GPIO Input Stage
+//-----------------------------------------------------------------------------
+// File          : gpio_input_stage.sv
+// Author        : Manuel Eggimann  <meggimann@iis.ee.ethz.ch>
+// Created       : 14.04.2022
+//-----------------------------------------------------------------------------
+// Description :
+//
+// This module implements the input synchronization stage for a single GPIO. It
+// uses a two-stage synchronizer for meta-stability resolution. This version of
+// the input stage instantiates a clock gate to disable input sampling when the
+// corresponding GPIO is disabled. This clock gate instance (tc_clk_gating) is
+// behavioraly implemented in the `pulp-plaform/common_cells` (on GitHub)
+// repository. In case you want to tape-out this GPIO, you either have to map
+// this behavioral clock gate cell to a dedicated ICG of your std cell library
+// or use the alternative version (`gpio_input_stage_no_clk_gates.sv`) of the
+// input stage that does not include any clock gates.
+//
+//-----------------------------------------------------------------------------
+// Copyright (C) 2022 ETH Zurich, University of Bologna
+// Copyright and related rights are licensed under the Solderpad Hardware
+// License, Version 0.51 (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+// or agreed to in writing, software, hardware and materials distributed under
+// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+// SPDX-License-Identifier: SHL-0.51
+//-----------------------------------------------------------------------------
+
+
 module gpio_input_stage #(
   parameter NrSyncStages=2
 ) (
